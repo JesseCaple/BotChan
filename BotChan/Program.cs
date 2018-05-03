dcd10c50e8557af733a295d7c775aed9416083a8
Jesse Caple <thisrhythm@gmail.com>
2018-05-02 23:00:27 -0600

    Add project files.

---
+++ BotChan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BotChan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
